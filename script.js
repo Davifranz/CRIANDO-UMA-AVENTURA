81384acc4421c92d21503272bb55aeb4b367426e
Davifranz <00001103769996sp@al.educacao.sp.gov.br>
2024-09-18 08:07:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const avanca = document.querySelectionAll('.btn-proximo');
 
-avanca.array.forEach(element => {
+avanca.array.forEach(button => {
+button.addEventListener('click', function(){
+const atual = document.querySelector('.ativo');
+const proximoPasso = 'passo-'+ this .getAttribute('data-proximo');
+
+atual.classList.remove('ativo');
+document.getElementById(proximoPasso).classList.add('ativo');
+})
     
 });
